166b28f7a8ff2e9d96ea9ff291607a50bbb62f74
Eliseo Luquez <eluquez@trabajo.gob.ar>
2021-09-09 19:41:20 -0300

    deployando lo nuevo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './componentes/error/error.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'registro', component:RegistroComponent},
+  {path:'quienSoy', component:QuienSoyComponent},
+  {path:'**', component:ErrorComponent},
+  {path:'', redirectTo:'home'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { ErrorComponent } from './componentes/error/error.component';
+import { MenuPrincipalComponent } from './componentes/menu-principal/menu-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    QuienSoyComponent,
+    RegistroComponent,
+    ErrorComponent,
+    MenuPrincipalComponent
   ],
   imports: [
     BrowserModule,
