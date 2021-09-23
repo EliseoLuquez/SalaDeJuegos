ab6d9340de31f9a68182d83680beb96c72a053bb
Eliseo Luquez <eluquez@trabajo.gob.ar>
2021-09-23 20:20:09 -0300

    arreglos home

--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -13,36 +13,78 @@ import { User } from 'src/app/shared/user';
 })
 export class HomeComponent implements OnInit {
 
-  usuario!: User;
+  usuario!: any;
   logueado: boolean = false;
-
+  mensaje!: string;
   constructor(public authSvc: AuthService, public router: Router, private ls: LocalStorageService) { }
 
   ngOnInit(): void {
   //   console.log(this.authSvc.usuario.logueado);
-  //   this.usuario = this.authSvc.checkIfUserIsLogged()
-  //   console.log(this.usuario.email);
+     this.usuario = this.authSvc.logueado;
+     console.log(this.usuario);
   //     if(this.usuario.email == this.ls.get('UserMail')){
   //       this.logueado = true;
   //   }
   }
 
   goPreguntados(){
-    this.router.navigate(['juegos/preguntados']);
+    if(this.usuario){
+      this.router.navigate(['juegos/preguntados']);
+    }
+    else{
+      this.mensaje = "Debes Loguearte!!";
+      console.log(this.mensaje);
+      
+    }
   }
   goMayorMenor(){
+    if(this.usuario){
     this.router.navigate(['juegos/mayormenor']);
   }
+  else{
+    this.mensaje = "Debes Loguearte!!";
+    console.log(this.mensaje);
+    
+  }
+  }
   goAhorcado(){
+    if(this.usuario){
     this.router.navigate(['juegos/ahorcado']);
   }
+  else{
+    this.mensaje = "Debes Loguearte!!";
+    console.log(this.mensaje);
+    
+  }
+  }
   goChat(){
+    if(this.usuario){
     this.router.navigate(['chat']);
   }
+  else{
+    this.mensaje = "Debes Loguearte!!";
+    console.log(this.mensaje);
+    
+  }
+  }
   goQuienSoy(){
+    if(this.usuario){
     this.router.navigate(['quienSoy']);
   }
+  else{
+    this.mensaje = "Debes Loguearte!!";
+    console.log(this.mensaje);
+    
+  }
+  }
   goJuegoPropio(){
+    if(this.usuario){
     this.router.navigate(['juegos/juegoPropio']);
+  }
+  else{
+    this.mensaje = "Debes Loguearte!!";
+    console.log(this.mensaje);
+    
+  }
   }
 }
